ee451428ea64885c22b21bed64bc4ca2ce3aab5f
nikhilareddy2420030387 <2420030387@klh.edu.in>
2025-07-26 11:32:03 +0530

    Add files via upload

---
+++ Increment.java
@@ -0,0 +1,21 @@
+package oops_practical;
+
+import java.util.Scanner;
+
+public class Increment {
+	public static void main(String args[]) {
+		Scanner myObj = new Scanner(System.in);
+		System.out.println("Enter a: ");
+		int a = myObj.nextInt();
+		System.out.println("Enter b: ");
+		int b = myObj.nextInt();
+		int result1 = ++a;
+		System.out.println("After increment: " + result1);
+		int result2 = --b;
+		System.out.println("After decrement: "+ result2);
+		myObj.close();
+		
+		
+	}
+
+}
---
+++ Operators.java
@@ -0,0 +1,5 @@
+package oops_practical;
+
+public class Operators {
+
+}
