1cde78f722a83f2d8cc42313f6fe6115fa197dfc
nikhilareddy2420030387 <2420030387@klh.edu.in>
2025-07-26 15:26:23 +0530

    Add files via upload

--- Operators.java
+++ Operators.java
@@ -1,5 +1,29 @@
 package oops_practical;
 
-public class Operators {
+import java.util.Scanner;
+
+public class Operators{
+	
+	public static void main(String args[]) {
+	Scanner myObj = new Scanner(System.in);
+	  System.out.println("Enter a: ");
+	  int a = myObj.nextInt(); 
+	  System.out.println("Enter b: ");
+	 int b = myObj.nextInt(); 
+	  System.out.println("a + b = " + (a + b));
+
+	   
+	    System.out.println("a - b = " + (a - b));
+
+	    System.out.println("a * b = " + (a * b));
+
+	   
+	    System.out.println("a / b = " + (a / b));
+
+	
+	    System.out.println("a % b = " + (a % b));
+	    myObj.close();
+	    
+}
 
 }
