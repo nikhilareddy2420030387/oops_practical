8ceb8c0af78085279903977322d63b0b3736b295
nikhilareddy2420030387 <2420030387@klh.edu.in>
2025-07-27 15:57:52 +0530

    Add files via upload

---
+++ Primitivedt.java
@@ -0,0 +1,20 @@
+package oops_practical;
+
+public class Primitivedt {
+	public static void main(String args[]) {
+		char a='N';
+		int i=5;
+		byte b=4;
+		short s=56;
+		double d=3.14245;
+		float f=4.5643f;
+		long l=123456;
+		System.out.println("char: "+a);
+		System.out.println("int: "+i);
+		System.out.println("byte: "+b);
+		System.out.println("short: "+s);
+		System.out.println("double: "+d);
+		System.out.println("float: "+f);
+		System.out.println("long: "+l);
+	}
+}
