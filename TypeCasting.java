afc0e0384527e21d153c9226763e1befd8532075
nikhilareddy2420030387 <2420030387@klh.edu.in>
2025-07-27 17:23:04 +0530

    Add files via upload

---
+++ TypeCasting.java
@@ -0,0 +1,16 @@
+package oops_practical;
+import java.util.Scanner;
+
+public class TypeCasting {
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);
+        System.out.print("Enter an integer value: ");
+        int i = myObj.nextInt();
+        long l = i;
+        double d = i;
+        System.out.println("Integer: " + i);
+        System.out.println("Long: " + l);
+        System.out.println("Double: " + d);
+        myObj.close();
+    }
+}
